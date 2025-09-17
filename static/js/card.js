5ab8d35832cee095671eac5ce0836817408b9437
DavidRojasGmb <davlidxrz6@gmail.com>
2025-09-17 06:47:26 +0000

    Primera version + sitemap

---
+++ static/js/card.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const botones = document.querySelectorAll(".ver-mas-btn");
+  botones.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const info = btn.closest("div").nextElementSibling;
+      info.classList.toggle("hidden");
+      btn.textContent = info.classList.contains("hidden") ? "Ver más" : "Ocultar";
+    });
+  });
+});
---
+++ static/js/carrusel.js
@@ -0,0 +1,23 @@
+
+const carousel = document.querySelector(".carousel-inner");
+const totalSlides = document.querySelectorAll(".carousel-item").length;
+let index = 0;
+
+function updateCarousel() {
+    carousel.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+    index = (index + 1) % totalSlides;
+    updateCarousel();
+}
+
+function prevSlide() {
+    index = (index - 1 + totalSlides) % totalSlides;
+    updateCarousel();
+}
+
+document.getElementById("next").addEventListener("click", nextSlide);
+document.getElementById("prev").addEventListener("click", prevSlide);
+
+setInterval(nextSlide, 3000); // Cambio automático cada 3 segundos
